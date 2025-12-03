abde1db46d8a683dad0e87573bcf6f0b2c8dfc30
h.mecca <hailemaryam.mecca@partner.safaricom.et>
2025-12-03 20:38:19 +0300

    test git hub action

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more, test commit for CI/CD
+        Click on the Vite and React logos to learn more
       </p>
     </>
   )
