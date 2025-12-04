ec9e393da85c53c54f09565f59cdcc399960aae0
Bethel Gelan <bethelgelan16@gmail.com>
2025-12-04 22:17:50 +0300

    Your commit message

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR, testing github actions
+          Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
