b025878e635f5bfa1ee1c5d3a189d49cf1f5727e
Bethel Gelan <bethelgelan16@gmail.com>
2025-12-05 14:19:46 +0300

    Sign up and sign in page

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
-import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import SignIn from "./pages/AuthPages/SignIn";
+import SignUp from "./pages/AuthPages/SignUp";
 
-
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      
-    
-    
-    </>
-  )
+    <Router>
+      <Routes>
+        {/* Auth Layout */}
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </Router>
+  );
 }
-
-export default App
